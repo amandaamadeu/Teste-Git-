5a62fd5bde49694e98d8efb101737542324ec051
aamadeu <amanda.amadeu@mercadolivre.com>
2024-06-24 15:11:32 -0300

    Arrumando bug sistema de login

---
+++ cadastro.js
@@ -0,0 +1,8 @@
+function signUp(email){ 
+    return email;
+}
+
+// pegando email e cadastrando na plataforma
+function MostraMensage(email){ 
+    return 'Bem vindo ao Sistema' ;
+}
--- login.js
+++ login.js
@@ -1,2 +1,6 @@
 //trabalhando no login //
-//finalizando trabalho de login // 
+//finalizando trabalho de login //
+Teste branch 
+
+// Arrumando sistema de Login do curso de Git 
+// Teste 123
